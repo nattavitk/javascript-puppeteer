f93992adc361f352a667db6cc163b7be5c539963
Nattavit Kamoltham <nattavitk@outlook.com>
2017-11-19 14:32:24 +0700

    alter test.js

--- test.js
+++ test.js
@@ -2,9 +2,15 @@ import puppeteer from 'puppeteer';
 import 'babel-polyfill';
 
 const getPic = async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
   const page = await browser.newPage();
   await page.goto('https://google.com');
+  await page.setViewport({
+    width: 1000,
+    height: 500,
+  });
   await page.screenshot({
     path: './screenshot/google.png',
   });
